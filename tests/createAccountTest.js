92fc15912bd397c7d350e5351c4e70a7d7d33d06
endqwerty <endqwerty@gmail.com>
2020-04-07 16:03:57 -0700

    working create new account test

--- tests/createAccountTest.js
+++ tests/createAccountTest.js
@@ -7,26 +7,28 @@ module.exports = {
             .waitForElementPresent('.login')
             .click('.login')
             .waitForElementPresent('#SubmitCreate')
-            .setValue('input#email_create', 'test+1@mail.com')
+            .setValue('input#email_create', 'test+4@mail.com')
             .click('#SubmitCreate')
             .assert.not.visible('#create_account_error')
             .waitForElementPresent('.account_creation')
     },
-    'Add use details': function (browser) {
+    'Add user details': function (browser) {
         browser
             .setValue('input#customer_firstname', 'Daniel')
             .setValue('input#customer_lastname', 'Yang')
             .setValue('input#passwd', 'P@ssw0rd!!!')
-            .setValue('input#firstname', 'Daniel')
-            .setValue('input#lastname', 'Yang')
+            // .setValue('input#firstname', 'Daniel')
+            // .setValue('input#lastname', 'Yang')
             .setValue('input#address1', '13 Cloverfield Ln')
             .setValue('input#city', 'Seattle')
             .setValue('select#id_state', 'Washington')
             .setValue('input#postcode', '12345')
             .setValue('select#id_country', 'United States')
             .setValue('input#phone_mobile', '1238675309')
-            .setValue('input#alias', 'address1')
-            .submitForm('button#submitAccount')
-        // .click('button#submitAccount')
+    },
+    'Submit new account': function (browser) {
+        browser
+            .click('button#submitAccount')
+            .waitForElementPresent('body#my-account')
     },
 }
