bee9b503ec9e9a6ac1c7b54719d1f1ba591c057a
endqwerty <endqwerty@gmail.com>
2020-04-07 15:57:23 -0700

    add file

---
+++ tests/createAccountTest.js
@@ -0,0 +1,32 @@
+module.exports = {
+    '@disabled': false,
+
+    'Find new account page': function (browser) {
+        browser
+            .url('http://www.automationpractice.com')
+            .waitForElementPresent('.login')
+            .click('.login')
+            .waitForElementPresent('#SubmitCreate')
+            .setValue('input#email_create', 'test+1@mail.com')
+            .click('#SubmitCreate')
+            .assert.not.visible('#create_account_error')
+            .waitForElementPresent('.account_creation')
+    },
+    'Add use details': function (browser) {
+        browser
+            .setValue('input#customer_firstname', 'Daniel')
+            .setValue('input#customer_lastname', 'Yang')
+            .setValue('input#passwd', 'P@ssw0rd!!!')
+            .setValue('input#firstname', 'Daniel')
+            .setValue('input#lastname', 'Yang')
+            .setValue('input#address1', '13 Cloverfield Ln')
+            .setValue('input#city', 'Seattle')
+            .setValue('select#id_state', 'Washington')
+            .setValue('input#postcode', '12345')
+            .setValue('select#id_country', 'United States')
+            .setValue('input#phone_mobile', '1238675309')
+            .setValue('input#alias', 'address1')
+            .submitForm('button#submitAccount')
+        // .click('button#submitAccount')
+    },
+}
